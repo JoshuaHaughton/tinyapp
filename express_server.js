57146f7417b07b6caeb60961d173ac21f641fcd8
Joshua Haughton <itsjoshuahaughton@gmail.com>
2021-11-08 23:20:27 +0000

    Implemented body parser, created a function to generate a random string to serve as a shortURL

--- express_server.js
+++ express_server.js
@@ -1,6 +1,22 @@
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
+function generateRandomString() {
+  let out = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  let out1 = out[(Math.random() * 35)]
+  let out2 = out[(Math.random() * 35)]
+  let out3 = out[(Math.random() * 35)]
+  let out4 = out[(Math.random() * 35)]
+  let out5 = out[(Math.random() * 35)]
+  let out6 = out[(Math.random() * 35)]
+  let final = `${out1}${out2}${out3}${out4}${out5}${out6}`
+  return final;
+}
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs')
 
@@ -34,6 +50,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
